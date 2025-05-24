e330643abb0c7ffdf18c633d09b82937e218fbbb
Alex <alexelenvarenko@gmail.com>
2025-05-24 10:53:40 +0300

    Move parse func to separated file

--- migration.go
+++ migration.go
@@ -3,8 +3,6 @@ package migration
 import (
 	"os"
 	"path/filepath"
-	"regexp"
-	"strings"
 	"time"
 )
 
@@ -31,28 +29,13 @@ func NewMigration(fileName string) *Migration {
 
 // Parse migration file
 func (m *Migration) Parse() {
-	context, err := os.ReadFile(m.fileName)
+	content, err := os.ReadFile(m.fileName)
 	if err != nil {
 		panic(err)
 	}
 
 	name := filepath.Base(m.fileName)
-	rUp := regexp.MustCompile(`(-- up --([^-]+)-- up --)`)
-	rDown := regexp.MustCompile(`(-- down --([^-]+)-- down --)`)
-
 	m.name = name
-
-	allSubMatches := rUp.FindAllStringSubmatch(string(context), -1)
-	if len(allSubMatches) > 0 {
-		if len(allSubMatches[0]) >= 2 {
-			m.up = strings.Trim(allSubMatches[0][2], "\n")
-		}
-	}
-
-	allSubMatches = rDown.FindAllStringSubmatch(string(context), -1)
-	if len(allSubMatches) > 0 {
-		if len(allSubMatches[0]) >= 2 {
-			m.down = strings.Trim(allSubMatches[0][2], "\n")
-		}
-	}
+	m.up = FindEntry(`(-- up --([^-]+)-- up --)`, string(content))
+	m.down = FindEntry(`(-- down --([^-]+)-- down --)`, string(content))
 }
